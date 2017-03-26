3571f3630ddd86b1d1fec84b1cd937a26e0add9c
J Mann <jmanndev@gmail.com>
2017-03-26 18:14:34 +1100

    Submit and Clear All buttons working

--- Exercise2/app/src/main/java/com/mad/exercise2/MainActivity.java
+++ Exercise2/app/src/main/java/com/mad/exercise2/MainActivity.java
@@ -1,13 +1,17 @@
 package com.mad.exercise2;
 
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
-	Button submitBtn;
+	Button submitBtn, clearAllBtn, exitBtn;
+	Snackbar snackbar;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -15,13 +19,33 @@ public class MainActivity extends AppCompatActivity {
 		setContentView(R.layout.activity_main);
 
 
-		submitBtn = (Button)findViewById(R.id.submitButton);
+		snackbar = Snackbar.make(findViewById(R.id.coordinatorLayout), getString(R.string.submitButtonSnackbar), Snackbar.LENGTH_LONG);
+
+		submitBtn = (Button) findViewById(R.id.submitButton);
+		clearAllBtn = (Button) findViewById(R.id.clearAllButton);
+		exitBtn = (Button) findViewById(R.id.exitButton);
+
 		submitBtn.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
-
+				snackbar.show();
 			}
 		});
+
+		clearAllBtn.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				clearAllForms();
+			}
+		});
+	}
+
+
+	private void clearAllForms() {
+		((EditText) findViewById(R.id.nameText)).setText("");
+		((EditText) findViewById(R.id.emailText)).setText("");
+		((EditText) findViewById(R.id.numberText)).setText("");
+		((Spinner) findViewById(R.id.numberTypeDropdown)).setSelection(0);
 	}
 
 
