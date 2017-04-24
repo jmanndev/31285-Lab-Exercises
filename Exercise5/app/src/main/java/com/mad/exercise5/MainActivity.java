2af34e2a068469927c412632145563f16d6749d2
jmanndev <jmanndev@gmail.com>
2017-04-24 10:59:29 +1000

    Exercise 5 completed

--- Exercise5/app/src/main/java/com/mad/exercise5/MainActivity.java
+++ Exercise5/app/src/main/java/com/mad/exercise5/MainActivity.java
@@ -13,18 +13,27 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import java.io.Console;
 import java.util.ArrayList;
 import java.util.Random;
 
+/**
+ * Main activity of a project to update/refresh Train arrival times.
+ * Created by Jonathan Mann 11393269
+ */
 public class MainActivity extends AppCompatActivity {
 
     private ArrayList<Train> trainsList = new ArrayList<>();
     private RecyclerView recyclerView;
     private TrainAdapter trainAdapter;
 
-
+    /**
+     * OnCreate method to assign click listeners, prepare recycler view, and prepare demo train data
+     *
+     * @param savedInstanceState Bundle that can be restored (not used for this project)
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,35 +49,45 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
         recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
-        trainAdapter = new TrainAdapter(this,trainsList);
+        trainAdapter = new TrainAdapter(this, trainsList);
 
         RecyclerView.LayoutManager trainLayoutManager = new LinearLayoutManager(getApplicationContext());
         recyclerView.setLayoutManager(trainLayoutManager);
         recyclerView.setItemAnimator(new DefaultItemAnimator());
         recyclerView.setAdapter(trainAdapter);
 
-
         prepareTrainData();
-
     }
 
+    /**
+     * Add sample train data to the train list.  Notify adapter of changes.
+     *
+     * The train status message is stored in strings.xml but all the others are hardcoded as they are placeholder data.
+     */
     private void prepareTrainData() {
         //String platform, int arrivalTime, String status, String destination, String destinationTime
-        Train train = new Train("Albion Park Platform 1", 3, "On time", "Allawah", "14:11");
-        trainsList.add(train);
 
-        trainsList.add(new Train("Arncliffe Platform 2", 4, "Late", "Central", "14:34"));
-        trainsList.add(new Train("Artarmon Platform 3", 7, "On time", "Ashfield", "15:01"));
+        trainsList.add(new Train("Albion Park Platform 1", 3, getString(R.string.train_status_ontime), "Allawah", "14:11"));
+        trainsList.add(new Train("Arncliffe Platform 2", 4, getString(R.string.train_status_late), "Central", "14:34"));
+        trainsList.add(new Train("Artarmon Platform 3", 7, getString(R.string.train_status_ontime), "Ashfield", "15:01"));
         addBerowraTrainDataToList();
         trainAdapter.notifyDataSetChanged();
     }
 
+    /**
+     * Add Berowra train data to list.  This is a separate function as the FAB adds copies of this train.
+     */
     private void addBerowraTrainDataToList() {
-        trainsList.add(new Train("Berowra Platform 4", 12, "Late", "Beverly", "15:18"));
+        trainsList.add(new Train("Berowra Platform 4", 12, getString(R.string.train_status_late), "Beverly", "15:18"));
     }
 
+    /**
+     * Creates the options menu
+     *
+     * @param menu
+     * @return
+     */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -76,6 +95,12 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
+    /**
+     * Called when an options item is selected.
+     *
+     * @param item The item selected
+     * @return
+     */
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
@@ -99,22 +124,37 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    /**
+     * Delete all train entries from list
+     */
     private void actionDeleteAll() {
         trainsList.clear();
         trainAdapter.notifyDataSetChanged();
     }
 
+    /**
+     * Empty placeholder class.  Could implement function to continue through the lifecycle to close app.
+     */
     private void actionQuit() {
 
     }
 
+    /**
+     * Calls AsyncTask to refresh all train arrival times
+     */
     private void actionRefresh() {
         new UpdateArrivalTimes().execute();
     }
 
+    /**
+     * Async class to generate random arrival times for all trains.
+     */
     private class UpdateArrivalTimes extends AsyncTask<Void, Void, String> {
         ProgressBar progressBar;
 
+        /**
+         * PreExecute: hide recycler view and show progress bar
+         */
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
@@ -124,12 +164,18 @@ public class MainActivity extends AppCompatActivity {
             recyclerView.setVisibility(View.INVISIBLE);
         }
 
+        /**
+         * Sleep for 3 seconds and generate random data.
+         *
+         * @param params
+         * @return
+         */
         @Override
         protected String doInBackground(Void... params) {
             try {
                 Thread.sleep(3000);
             } catch (InterruptedException e) {
-                e.printStackTrace();
+                //I don't believe there is an appropriate way to handle this exception as the code will continue regardless if the timer fails.
             }
 
             Random rand = new Random();
@@ -140,6 +186,11 @@ public class MainActivity extends AppCompatActivity {
             return null;
         }
 
+        /**
+         * OnPostExecute: show recycler view and hide progress bar
+         *
+         * @param s
+         */
         @Override
         protected void onPostExecute(String s) {
             super.onPostExecute(s);
@@ -148,4 +199,5 @@ public class MainActivity extends AppCompatActivity {
             trainAdapter.notifyDataSetChanged();
         }
     }
+
 }
--- Exercise5/app/src/main/java/com/mad/exercise5/TrainAdapter.java
+++ Exercise5/app/src/main/java/com/mad/exercise5/TrainAdapter.java
@@ -2,104 +2,170 @@ package com.mad.exercise5;
 
 import android.content.Context;
 import android.os.AsyncTask;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-
+/**
+ * Train adapter class for adapter/recyclerView implementation
+ */
 public class TrainAdapter extends RecyclerView.Adapter<TrainAdapter.ViewHolder> {
 
     private List<Train> trainsList;
     private Context context;
+    public TextView platform, status, destinationTime, arrivalTime, destination;
 
-    public class ViewHolder extends RecyclerView.ViewHolder {
-        public TextView platform, status, destinationTime, arrivalTime, destination;
+    /**
+     * Class for a single row of data in adapter
+     */
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
         public ViewHolder(View itemView) {
             super(itemView);
+            itemView.findViewById(R.id.train_time_layout).setOnClickListener(this);
             platform = (TextView) itemView.findViewById(R.id.textview_platform);
             status = (TextView) itemView.findViewById(R.id.textview_status);
             destination = (TextView) itemView.findViewById(R.id.textview_destination);
             destinationTime = (TextView) itemView.findViewById(R.id.textview_destination_time);
             arrivalTime = (TextView) itemView.findViewById(R.id.textview_arrival_time);
-            itemView.findViewById(R.id.train_time_layout).setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    new UpdateArrivalTime(v).execute();
-                }
-            });
+        }
+
+        /**
+         * OnClick for updating arrival time
+         *
+         * @param v View
+         */
+        @Override
+        public void onClick(View v) {
+            new UpdateArrivalTime(v, getAdapterPosition()).execute();
         }
     }
 
+    /**
+     * Constructor to assign parameters to class variables
+     *
+     * @param context
+     * @param trainsList
+     */
     public TrainAdapter(Context context, ArrayList<Train> trainsList) {
         this.trainsList = trainsList;
         this.context = context;
     }
 
+    /**
+     * Inflates content to display in view holder
+     *
+     * @param parent
+     * @param viewType
+     * @return
+     */
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.train_item,parent,false);
-
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.train_item, parent, false);
         return new ViewHolder(itemView);
     }
 
+    /**
+     * Binds content in list to view holder row
+     *
+     * @param holder
+     * @param position
+     */
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
         Train train = trainsList.get(position);
-        holder.platform.setText(train.getPlatform());
-        holder.status.setText(train.getStatus());
-        holder.destination.setText(train.getDestination​());
-        holder.destinationTime.setText(train.getDestinationTime​());
-        holder.arrivalTime.setText(Integer.toString(train.getArrivalTime()) + "\n" + context.getString(R.string.arrival_time_min_suffix));
+        platform.setText(train.getPlatform());
+        status.setText(train.getStatus());
+        destination.setText(train.getDestination​());
+        destinationTime.setText(train.getDestinationTime​());
+        arrivalTime.setText(Integer.toString(train.getArrivalTime()) + "\n" + context.getString(R.string.arrival_time_min_suffix));
+
+        if (train.getStatus().equals(context.getString(R.string.train_status_late))) {
+            status.setTextColor(ContextCompat.getColor(context, R.color.colorLateText));
+        }
     }
 
+    /**
+     * Gets the item count of the list
+     *
+     * @return
+     */
     @Override
     public int getItemCount() {
         return trainsList.size();
     }
 
-    private class UpdateArrivalTime extends AsyncTask<Void,Void, Integer> {
+    /**
+     * Async Task to refresh a single arrival time
+     */
+    private class UpdateArrivalTime extends AsyncTask<Void, Void, Integer> {
 
         View view;
         ProgressBar progressBar;
         TextView arrivalTextView;
+        int position;
 
-        public UpdateArrivalTime(View itemView) {
-            view=itemView;
+        /**
+         * Constructor to assign parameters to local variables
+         *
+         * @param itemView
+         * @param position
+         */
+        public UpdateArrivalTime(View itemView, int position) {
+            view = itemView;
+            this.position = position;
         }
 
+        /**
+         * Pre execute to hide arrival text and show progress bar
+         */
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
-            progressBar = (ProgressBar)view.findViewById(R.id.train_progress_bar);
+            progressBar = (ProgressBar) view.findViewById(R.id.train_progress_bar);
             progressBar.setVisibility(View.VISIBLE);
-            arrivalTextView = (TextView)view.findViewById(R.id.textview_arrival_time);
+            arrivalTextView = (TextView) view.findViewById(R.id.textview_arrival_time);
             arrivalTextView.setVisibility(View.INVISIBLE);
         }
 
+        /**
+         * Sleep 2 seconds and generate random number for arrival time
+         *
+         * @param params
+         * @return random arrival time
+         */
         @Override
         protected Integer doInBackground(Void... params) {
             try {
                 Thread.sleep(2000);
             } catch (InterruptedException e) {
-                e.printStackTrace();
+                //I don't believe there is an appropriate way to handle this exception as the code will continue regardless if the timer fails.
             }
 
             return new Random().nextInt(20) + 1;
         }
 
+        /**
+         * Assign paramater to train list, update view and hide progress bar.
+         *
+         * @param arrivalTime random arrival time
+         */
         @Override
-        protected void onPostExecute(Integer integer) {
-            super.onPostExecute(integer);
+        protected void onPostExecute(Integer arrivalTime) {
+            super.onPostExecute(arrivalTime);
+            trainsList.get(position).setArrivalTime(arrivalTime);
+            progressBar.setVisibility(View.GONE);
+            arrivalTextView.setVisibility(View.VISIBLE);
+            notifyDataSetChanged();
         }
     }
 
